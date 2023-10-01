9fd3f0f4d22fdb5e1aa5879b56e6e08297c6dda0
Assaf <assafyechiel@gmail.com>
2023-10-01 14:24:55 +0300

    Add class 'horizontal'

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector("#container");
 
 const horizontal = document.createElement("div");
+horizontal.classList.add("horizontal");
 
 const square = document.createElement("div");
