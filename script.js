f64f40898cbf093d91342bbcf0fb0aacd0720d80
Assaf <assafyechiel@gmail.com>
2023-10-01 17:55:05 +0300

    Add counter variable

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const container = document.querySelector('.container');
 
-for (i = 0; i < 2; i++) {
+let counter = 3;
+
+for (i = 0; i < counter; i++) {
     const horizontal = document.createElement('div');
     horizontal.classList.add('horizontal');
     horizontal.setAttribute('id', i);
     container.appendChild(horizontal);
 
-    for (j = 0; j < 2; j++) {
+    for (j = 0; j < counter; j++) {
         const square = document.createElement('div');
         square.classList.add('square');
         square.setAttribute('id', j);
