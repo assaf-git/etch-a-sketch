ab7f58bb19508e11f6be4883c32aac4f00c674ff
Assaf <assafyechiel@gmail.com>
2023-10-02 15:10:40 +0300

    Create squareNum array

--- script.js
+++ script.js
@@ -9,15 +9,16 @@ let counter = 16;
 for (i = 0; i < counter; i++) {
     const horizontal = document.createElement('div');
     horizontal.classList.add('horizontal');
-    horizontal.setAttribute('id', i);
+    horizontal.setAttribute('id', i); //May be irrelevant
     gridShell.appendChild(horizontal);
 
     for (j = 0; j < counter; j++) {
         const squares = document.createElement('div');
         squares.classList.add('square');
-        squares.setAttribute('id', j);
+        const squareNum = [(i + 1) * (j + 1)];
+        squares.setAttribute('id', squareNum);
         horizontal.appendChild(squares);
+        console.log(squareNum);
     }
     
 }
-
