5a94ad5db84201b998c8e8e5e262d38fac24b27b
Assaf <assafyechiel@gmail.com>
2023-10-03 15:06:01 +0300

    Create event listener for slider

--- script.js
+++ script.js
@@ -16,40 +16,49 @@ sliderContainer.appendChild(slider);
 sliderContainer.appendChild(gridOutput);
 container.appendChild(sliderContainer);
 
-gridOutput.textContent = "Value: " + slider.value;
 
-slider.oninput = function () {
-    gridOutput.textContent = slider.value;
-}
-
-// let counter = 8;
+slider.addEventListener('change', function(){
+    gridOutput.textContent = "Value: " + slider.value;
+    console.log(gridOutput);
+    console.log(slider.value);
+    gridAssembly();
+})
 
 let gridCounter = 0; //May be irrelevant
 
 const gridSquares = [];
 
+gridAssembly();
+
+gridOutput.textContent = "Value: " + slider.value;
 
 
-for (i = 0; i < slider.value; i++) {
-    const horizontal = document.createElement('div');
-    horizontal.classList.add('horizontal');
-    horizontal.setAttribute('id', i); //May be irrelevant
-    gridShell.appendChild(horizontal);
+function gridAssembly() {
 
-    for (j = 0; j < slider.value; j++) {
-        const squares = document.createElement('div');
-        squares.classList.add('square');
-        squares.setAttribute('id', j); //May be irrelevant
-        horizontal.appendChild(squares);
-        gridSquares.push(squares);
-        gridCounter++; //May be irrelevant
+    for (i = 0; i < slider.value; i++) {
+        const horizontal = document.createElement('div');
+        horizontal.classList.add('horizontal');
+        horizontal.setAttribute('id', i); //May be irrelevant
+        gridShell.appendChild(horizontal);
+    
+        for (j = 0; j < slider.value; j++) {
+            const squares = document.createElement('div');
+            squares.classList.add('square');
+            squares.setAttribute('id', j); //May be irrelevant
+            horizontal.appendChild(squares);
+            gridSquares.push(squares);
+            gridCounter++; //May be irrelevant
+        }
     }
 }
 
-// console.log(gridSquares);
 
 gridSquares.forEach(gridSquare => {
     gridSquare.addEventListener('mouseover', () => {
         gridSquare.style.backgroundColor = 'black';
     });
 });
+
+function playGame() {
+
+}
